f3079d65119d156a3f8d6834ecf3b7e6be4f14e5
Zsofia Szonyi <szonyi.zsofia@gmail.com>
2020-04-28 15:02:30 +0100

    Add post route for named-cat

--- app.rb
+++ app.rb
@@ -23,9 +23,13 @@ get "/random-cat" do
   erb(:index)
 end
 
-get "/named-cat" do
+get "/cat-form" do
+  p params
+  erb(:cat_form)
+end
+
+post "/named-cat" do
   p params
   @name = params[:name]
   erb(:index)
-
 end
